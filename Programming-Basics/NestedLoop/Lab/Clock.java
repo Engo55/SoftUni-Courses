bfb60a676f1c86ba790a0a22d77b27812cf0d18f
Engo55 <engin655@abv.bg>
2021-08-23 23:13:18 +0300

    Added NestedLoops

---
+++ Programming-Basics/NestedLoop/Lab/Clock.java
@@ -0,0 +1,15 @@
+package NestedLoop.Lab;
+
+import java.util.Scanner;
+
+public class Clock {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int hour = 0; hour <= 23; hour++) {
+            for (int minute = 0; minute <= 59; minute++) {
+                System.out.printf("%d:%d%n", hour, minute);
+            }
+        }
+    }
+}
---
+++ Programming-Basics/NestedLoop/Lab/Travelling.java
@@ -0,0 +1,11 @@
+package NestedLoop.Lab;
+
+import java.util.Scanner;
+
+public class Travelling {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
