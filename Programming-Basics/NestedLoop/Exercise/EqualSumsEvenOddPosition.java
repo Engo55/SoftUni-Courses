33241cc11687de45f2fe3bb977991f841717ff10
Engo55 <engin655@abv.bg>
2022-02-05 01:50:35 +0200

    Added Exercise NestedLoop.

---
+++ Programming-Basics/NestedLoop/Exercise/EqualSumsEvenOddPosition.java
@@ -0,0 +1,29 @@
+package NestedLoop.Exercise;
+
+import java.util.Scanner;
+
+public class EqualSumsEvenOddPosition {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int first = Integer.parseInt(scanner.nextLine());
+        int second = Integer.parseInt(scanner.nextLine());
+
+        for (int i = first; i <= second; i++) {
+            String currentNum = "" + i;
+            int evenSum = 0;
+            int oddSum = 0;
+            for (int j = 0; j < currentNum.length(); j++) {
+                int currentDigit = Integer.parseInt("" + currentNum.charAt(j));
+                if (j % 2 == 0) {
+                    evenSum += currentDigit;
+                } else {
+                    oddSum += currentDigit;
+                }
+            }
+            if (oddSum == evenSum) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
---
+++ Programming-Basics/NestedLoop/Exercise/TrainTheTrainers.java
@@ -0,0 +1,34 @@
+package NestedLoop.Exercise;
+
+import java.util.Scanner;
+
+public class TrainTheTrainers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        String line = scanner.nextLine();
+
+        double allGradesSum = 0.0;
+        int countPresentations = 0;
+
+        while (!line.equals("Finish")) {
+            String presentation = line;
+            countPresentations++;
+
+            double gradesSum = 0.0;
+            double avrGrade = 0.0;
+
+            for (int i = 0; i < n; i++) {
+                double grade = Double.parseDouble(scanner.nextLine());
+                gradesSum += grade;
+            }
+            avrGrade = gradesSum / n;
+            allGradesSum += avrGrade;
+            System.out.printf("%s - %.2f.%n", presentation, avrGrade);
+
+            line = scanner.nextLine();
+        }
+        System.out.printf("Student's final assessment is %.2f.", allGradesSum / countPresentations);
+    }
+}
